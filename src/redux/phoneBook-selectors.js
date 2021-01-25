ab29e3893e1fdcaec5b4b97ad4b3ebc716f6fde5
Yurii Zubar <barsuk0831@gmail.com>
2021-01-25 13:30:40 +0200

    memoizze

--- src/redux/phoneBook-selectors.js
+++ src/redux/phoneBook-selectors.js
@@ -1,9 +1,13 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getContacts = state => state.phoneBook.items;
 export const getFilter = state => state.phoneBook.filter;
 
-export const getVisibleContacts = state => {
-  const items = getContacts(state);
-  const filter = getFilter(state);
-  const normalizeFilter = filter.toLowerCase();
-  return items.filter(item => item.data.name.includes(normalizeFilter));
-};
+export const getVisibleContacts = createSelector(
+  [getContacts, getFilter],
+  (items, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return items.filter(item => item.data.name.includes(normalizedFilter));
+  },
+);
