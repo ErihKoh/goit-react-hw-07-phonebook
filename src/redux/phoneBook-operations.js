84726ab08635a5f1914aa315472f77f8da6e0de7
Yurii Zubar <barsuk0831@gmail.com>
2021-01-30 12:46:21 +0200

    change on async await

--- src/redux/phoneBook-operations.js
+++ src/redux/phoneBook-operations.js
@@ -13,29 +13,37 @@ import {
 
 axios.defaults.baseURL = 'http://localhost:4040';
 
-export const fetchContacts = () => dispatch => {
+export const fetchContacts = () => async dispatch => {
   dispatch(fetchContactRequest());
 
-  axios
-    .get('/contacts')
-    .then(({ data }) => dispatch(fetchContactSuccess(data)))
-    .catch(error => dispatch(fetchContactError(error)));
+  try {
+    const { data } = await axios.get('/contacts');
+    dispatch(fetchContactSuccess(data));
+  } catch (error) {
+    dispatch(fetchContactError(error.message));
+  }
 };
 
-export const addContacts = data => dispatch => {
+export const addContacts = data => async dispatch => {
   dispatch(addContactRequest());
 
-  axios
-    .post('/contacts', { data })
-    .then(({ data }) => dispatch(addContactSuccess(data)))
-    .catch(error => dispatch(addContactError(error)));
+  try {
+    const response = await axios.post('/contacts', { data });
+    console.log(response);
+    dispatch(addContactSuccess(response.data));
+  } catch (error) {
+    dispatch(addContactError(error.message));
+  }
 };
 
-export const deleteContact = id => dispatch => {
+export const deleteContact = id => async dispatch => {
   dispatch(deleteContactRequest());
 
-  axios
-    .delete(`contacts/${id}`)
-    .then(dispatch(deleteContactSuccess(id)))
-    .catch(error => dispatch(deleteContactError(error)));
+  try {
+    await axios.delete(`contacts/${id}`);
+
+    dispatch(deleteContactSuccess(id));
+  } catch (error) {
+    dispatch(deleteContactError(error.message));
+  }
 };
